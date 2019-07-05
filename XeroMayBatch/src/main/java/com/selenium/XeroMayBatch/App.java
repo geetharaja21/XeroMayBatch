b379a3e3f43ab703b46f82f01c1a95d3a42d88c1
geetharaja <p.r.geetha@gmail.com>
2019-07-04 20:37:35 -0700

    This is my first commit

---
+++ XeroMayBatch/src/main/java/com/selenium/XeroMayBatch/App.java
@@ -0,0 +1,13 @@
+package com.selenium.XeroMayBatch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ XeroMayBatch/src/test/java/com/selenium/XeroMayBatch/AutomateXero.java
@@ -0,0 +1,10 @@
+package com.selenium.XeroMayBatch;
+
+public class AutomateXero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Xero");
+	}
+
+}
